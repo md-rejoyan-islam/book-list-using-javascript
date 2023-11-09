26be5ececa8d52e7b38c26ebaeb43f768afc6dc1
RejoyanIslam <rejoyanislam0014@gmail.com>
2023-11-09 06:35:41 +0600

    update js file

--- js/app.js
+++ js/app.js
@@ -7,23 +7,84 @@ class Book {
   }
 }
 
+class UI {
+  static showAlert(message, type) {
+    const error = document.querySelector(".error");
+    error.classList.remove("hidden");
+    if (type === "success") {
+      error.classList.add("bg-green-500");
+      error.classList.remove("bg-red-500");
+      error.textContent = message;
+    } else {
+      error.classList.add("bg-red-500");
+      error.classList.remove("bg-green-500");
+      error.textContent = message;
+    }
+    setTimeout(() => error.classList.add("hidden"), 3000);
+  }
+  static isbnCheck(isbn) {
+    const result = Store.getBooks().find((book) => book.isbn === isbn);
+    if (result) {
+      return this.showAlert("This ISBN number already exists", "error");
+    }
+  }
+  static addBookToList(book) {
+    const bookList = document.querySelector("#book-list");
+    const row = document.createElement("tr");
+    row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td>
+        <button
+            class="py-2 px-4 rounded-md border focus:outline-none border-zinc-200 uppercase font-semibold text-xs text-white bg-red-400 hover:bg-red-500 delete"
+        >
+            Delete
+        </button>
+        </td>
+    `;
+    bookList.appendChild(row);
+  }
+}
+
+class Store {
+  static getBooks() {
+    let books = localStorage.getItem("books")
+      ? JSON.parse(localStorage?.getItem("books"))
+      : [];
+    return books;
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+    books.forEach((book, index) => {
+      if (book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+
+  static displayBooks() {
+    const books = Store.getBooks();
+    books.forEach((book) => {
+      UI.addBookToList(book);
+    });
+  }
+}
+
 // query selectors
-const error = document.querySelector(".error");
 const bookAddForm = document.querySelector("#book-add-form");
 const bookList = document.querySelector("#book-list");
+document.addEventListener("DOMContentLoaded", Store.displayBooks);
 
 // event listeners
 bookAddForm.addEventListener("submit", addBook);
-
-// books list
-const booksList = localStorage?.getItem("books")
-  ? JSON.parse(localStorage.getItem("books"))
-  : [];
-
-// before data show
-if (booksList.length > 0) {
-  booksLoop(booksList);
-}
 
 // add book function
 function addBook(e) {
@@ -35,62 +96,26 @@ function addBook(e) {
 
   // check empty value
   if (title === "" || author === "" || isbn === "") {
-    error.textContent = "Please fill in all fields";
-    error.classList.remove("hidden");
-    setTimeout(() => error.classList.add("hidden"), 3000);
+    UI.showAlert("Please fill in all fields", "error");
   } else {
     const book = new Book(title, author, isbn);
     // check isbn number
-    const isbnCheck = booksList.find((book) => book.isbn === isbn);
+    UI.isbnCheck(isbn);
+    // add book to list
+    UI.addBookToList(book);
+    // add book to local storage
+    Store.addBook(book);
 
-    // if isbn number already exists
-    if (isbnCheck) {
-      error.textContent = "This ISBN number already exists";
-      error.classList.remove("hidden");
-      setTimeout(() => error.classList.add("hidden"), 3000);
-      return;
-    }
-
-    booksList.push(book);
-    // books data show
-    booksLoop(booksList);
-
-    // save to local storage
-    localStorage.setItem("books", JSON.stringify(booksList));
+    // success message
+    UI.showAlert("Book Added", "success");
 
     // form reset
     e.target.reset();
   }
 }
 
-// books loop
-function booksLoop(books) {
-  let lists = "";
-  books.forEach((book) => {
-    lists += `<tr class="my-3">
-        <td>${book.title}</td>
-        <td>${book.author}</td>
-        <td>${book.isbn}</td>
-        <td>
-        <button
-            class="py-2 px-4 rounded-md border focus:outline-none border-zinc-200 uppercase font-semibold text-xs text-white bg-red-400 hover:bg-red-500 delete"
-        >
-            Delete
-        </button>
-        </td>
-    </tr>`;
-  });
-  bookList.innerHTML = lists;
-}
-
 // delete book
 bookList.addEventListener("click", (e) => {
   e.target.parentElement.parentElement.remove();
-  console.log(booksList);
-  const newList = booksList.filter((book) => {
-    return (
-      book.isbn !== e.target.parentElement.previousElementSibling.textContent
-    );
-  });
-  localStorage.setItem("books", JSON.stringify(newList));
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 });
